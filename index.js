d41ad08ca68730248ba44bb5b15e14d8dc76c5d1
mohdtajul <mohdtajul473@gmail.com>
2025-10-25 11:44:28 +0530

    Initial commit - REST API mini project

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require("express")
+const app = express()
+
+const port = 8080;
+
+const path = require("path")
+const { v4 :uuidv4 } = require('uuid');
+const methodOverride = require('method-override')
+
+let posts= [
+    {   id: uuidv4(),
+        username: "mohd tajul",
+        content: "i love coding"
+    },
+     {  id: uuidv4(),
+        username: "mohd talib",
+        content: "i love gaming"
+    },
+     {  id: uuidv4(),
+        username: "mohd kamil",
+        content: "i love teaching"
+    }
+]
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname,"views"))
+app.use(express.static(path.join(__dirname,"public")))
+
+/// REST API Calls 
+app.get("/posts", (req,res)=>{
+    // res.send("server is running well!")
+    res.render("index.ejs", {posts})
+
+});
+
+app.get("/posts/new", (req,res)=>{
+    res.render("new.ejs")
+});
+
+app.post("/posts",(req,res)=>{
+    let {username, content} = req.body;
+    let id = uuidv4();
+    posts.push({id, username, content});
+    res.redirect("/posts")
+});
+
+app.get("/posts/:id", (req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id);
+    // console.log(post);
+    // res.send("request is working")
+    res.render("show.ejs", {post})
+});
+
+app.patch("/posts/:id", (req,res)=>{
+    let {id} = req.params;
+    console.log(id);
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id === p.id);
+    if (!post) {
+        return res.status(404).send("Post not found");
+    }
+    post.content = newContent;
+    console.log(post);
+    res.redirect('/posts');
+})
+
+app.get("/posts/:id/edit", (req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id);
+    res.render("edit.ejs", {post})
+
+
+})
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p)=> id !== p.id);
+    // res.send("delete is working ")
+    res.redirect("/posts")
+
+
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+});
